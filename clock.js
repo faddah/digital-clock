0fb095936587d3269abfff600913b0adb81d710a
Faddah Wolf <my_biz@me.com>
2025-01-17 13:12:39 -0800

    Added proper code so clock shows time right away, not half-a-second of "00."

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@ const hrs = document.querySelector(`#hrs`);
 const min = document.querySelector(`#min`);
 const sec = document.querySelector(`#sec`);
 
+/* * * * * * * * * * * * * * * * * * * * * * *
 setInterval(() => {
     let currentTime = new Date();
 
@@ -11,6 +12,21 @@ setInterval(() => {
     min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
+* * * * * * * * * * * * * * * * * * * * * * */
+
+const updateTime = () => {
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}
+
+// Call updateTime once to set the initial time
+updateTime();
+
+// Set interval to update the time every second
+setInterval(updateTime, 1000);
 
 /* * * * * * * * * * * * * * * * * * * * * * * 
 console.log(`currentTime: ${currentTime}`);
